ba2480514a5e2c9849a7c1912b51498992460d18
OrthogonalHawk <OrthogonalHawk@gmail.com>
2014-02-28 03:16:48 +0000

    Adding an addition test for the LargeNumber class.

--- project-euler/Problem025/LargeNumberTest.cpp
+++ project-euler/Problem025/LargeNumberTest.cpp
@@ -6,7 +6,32 @@
 TEST(LargeNumber, Constructor)
 {
     LargeNumber myNumber;
-    EXPECT_EQ(1, 1);
+
+    EXPECT_EQ("0", myNumber.toString());
+}
+
+TEST(LargeNumber, Initialization)
+{
+    LargeNumber myNumber;
+    myNumber.initialize(10);
+    EXPECT_EQ("10", myNumber.toString());
+}
+
+TEST(LargeNumber, Addition)
+{
+    LargeNumber myNumber1;
+    LargeNumber myNumber2;
+
+    myNumber1.initialize(1);
+    myNumber2.initialize(3);
+
+    LargeNumber mySum = myNumber1 + myNumber2;
+
+    EXPECT_EQ("4", mySum.toString());
+
+    mySum = mySum + myNumber2 + myNumber2;
+
+    EXPECT_EQ("10", mySum.toString());
 }
 
 int main(int argc, char **argv)
