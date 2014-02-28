d9fd387a113741b940b546cf1320779b47bd512e
OrthogonalHawk <OrthogonalHawk@gmail.com>
2014-02-28 03:29:46 +0000

    Updated main function to calculate solution.

--- project-euler/Problem025/Problem025.cpp
+++ project-euler/Problem025/Problem025.cpp
@@ -1,8 +1,37 @@
 #include <iostream>
+#include <stdint.h>
+
+#include "LargeNumber.h"
+
+static const uint32_t TARGET_FIB_LENGTH = 1000;
 
 int main(void)
 {
-    std::cout << "Hello World!" << std::endl;
-  
+    LargeNumber fibN;
+    LargeNumber fibNP1;
+    LargeNumber nextFib;
+    uint32_t fibTerm = 2;
+
+    fibN.initialize(1);
+    fibNP1.initialize(1);
+
+
+
+    while (nextFib.getNumberLength() < TARGET_FIB_LENGTH)
+    {
+        nextFib = fibN + fibNP1;
+
+        fibN = fibNP1;
+        fibNP1 = nextFib;
+
+        ++fibTerm;
+    }
+
+    std::cout << "The first Fibonacci number with more than " << TARGET_FIB_LENGTH
+              << " digits is: " << nextFib.toString() << std::endl;
+
+    std::cout << "This number is the " << fibTerm 
+              << " number in the Fibonacci series." << std::endl;
+
     return 0;
 }
