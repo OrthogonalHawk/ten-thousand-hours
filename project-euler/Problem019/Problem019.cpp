5dd6a08b663757f173045454c21eb679fe97553a
OrthogonalHawk <OrthogonalHawk@gmail.com>
2014-03-03 04:30:19 +0000

    Adding solution for Problem019

--- project-euler/Problem019/Problem019.cpp
+++ project-euler/Problem019/Problem019.cpp
@@ -1,8 +1,10 @@
 
 #include <iostream>
+#include <sstream>
 #include <stdint.h>
 
-enum {
+enum MONTH_IN_YEAR 
+{
     JAN = 0,
     FEB,
     MAR,
@@ -15,23 +17,26 @@ enum {
     OCT,
     NOV,
     DEC
-} MONTH;
+};
 
-enum {
-    SUNDAY,
+enum DAY_OF_WEEK
+{
+    SUNDAY = 0,
     MONDAY,
     TUESDAY,
     WEDNESDAY,
     THURSDAY,
     FRIDAY,
     SATURDAY
-} DAY;
+};
 
-struct {
+struct DAY_TRACKER
+{
     uint32_t year;
-    MONTH month;
-    DAY day;
-} DAY_TRACKER;
+    MONTH_IN_YEAR month;
+    uint32_t day;
+    DAY_OF_WEEK weekday;
+};
 
 bool is_leap_year(uint32_t year)
 {
@@ -56,7 +61,18 @@ struct DAY_TRACKER get_next_day(struct DAY_TRACKER current_day)
     struct DAY_TRACKER new_day = current_day;
     bool increment_month = false;
 
-    switch (current_day.month)
+    switch (new_day.weekday)
+    {
+        case SUNDAY: new_day.weekday = MONDAY; break;
+        case MONDAY: new_day.weekday = TUESDAY; break;
+        case TUESDAY: new_day.weekday = WEDNESDAY; break;
+        case WEDNESDAY: new_day.weekday = THURSDAY; break;
+        case THURSDAY: new_day.weekday = FRIDAY; break;
+        case FRIDAY: new_day.weekday = SATURDAY; break;
+        case SATURDAY: new_day.weekday = SUNDAY; break;
+    }
+
+    switch (new_day.month)
     {
         case FEB:
             if (is_leap_year(new_day.year))
@@ -90,6 +106,7 @@ struct DAY_TRACKER get_next_day(struct DAY_TRACKER current_day)
 
         case JAN:
         case MAR:
+        case MAY:
         case JULY:
         case AUG:
         case OCT:
@@ -100,13 +117,71 @@ struct DAY_TRACKER get_next_day(struct DAY_TRACKER current_day)
                 increment_month = true;
             }
             break;
+
+        default:
+            std::cout << "ERROR - unhandled month!" << std::endl;
+            break;
+    }
+
+    if (increment_month)
+    {
+        switch (new_day.month)
+        {
+            case JAN: new_day.month = FEB; break;
+            case FEB: new_day.month = MAR; break;
+            case MAR: new_day.month = APRIL; break;
+            case APRIL: new_day.month = MAY; break;
+            case MAY: new_day.month = JUNE; break;
+            case JUNE: new_day.month = JULY; break;
+            case JULY: new_day.month = AUG; break;
+            case AUG: new_day.month = SEPT; break;
+            case SEPT: new_day.month = OCT; break;
+            case OCT: new_day.month = NOV; break;
+            case NOV: new_day.month = DEC; break;
+            case DEC: new_day.month = JAN;
+                new_day.year++;
+                break;
+        }
     }
 
     return new_day;
 }
 
+std::string print_day(DAY_TRACKER day)
+{
+    std::stringstream output;
+
+    output << "Year: " << day.year << " Month: " << day.month << " Day: " << day.day
+           << " Day of Week: " << day.weekday;
+
+    return output.str();
+}
+
 int main(void)
 {
+    struct DAY_TRACKER cur_day;
+    uint32_t num_Sundays = 0;
+
+    // initialize the day
+    cur_day.weekday = MONDAY;
+    cur_day.year = 1900;
+    cur_day.day = 1;
+    cur_day.month = JAN;
+
+    while (cur_day.year < 2001)
+    {
+        cur_day = get_next_day(cur_day);
+
+        if (cur_day.year >= 1901)
+        {
+            if (cur_day.day == 1 && cur_day.weekday == SUNDAY)
+                num_Sundays++;
+        }
+    }
+
+    std::cout << "Found " << num_Sundays << " Sundays." << std::endl;
+    std::cout << " Final day: " << print_day(cur_day) << std::endl;
+
     return 0;
 }
 
